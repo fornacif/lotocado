428932a434b3aa6e0217d7778079f5754dcc2ed7
François Fornaciari <francois.fornaciari@gmail.com>
2013-10-02 14:38:28 +0200

    Clean code

--- war/js/translate.js
+++ war/js/translate.js
@@ -2,27 +2,20 @@
 
 angular.module("lotocado.translate", ["pascalprecht.translate"]).
 	config(["$translateProvider", function ($translateProvider) {
-		$translateProvider.translations('en', {
-			CONTACT_US: 'Contact us',
-			WELCOME_TO: 'Welcome to',
-			CREATE_EVENT: 'Create event'
+		$translateProvider.translations("en", {
+			CONTACT_US: "Contact us",
+			WELCOME_TO: "Welcome to",
+			CREATE_EVENT: "Create event"
 		});
-		$translateProvider.translations('fr', {
-			CONTACT_US: 'Nous contacter',
-			WELCOME_TO: 'Bienvenue dans',
-			CREATE_EVENT: 'Cr&eacute;er un &eacute;v&egrave;nement'
+		$translateProvider.translations("fr", {
+			CONTACT_US: "Nous contacter",
+			WELCOME_TO: "Bienvenue dans",
+			CREATE_EVENT: "Cr&eacute;er un &eacute;v&egrave;nement"
 		});
-		$translateProvider.preferredLanguage('en');
+		$translateProvider.preferredLanguage("fr");
 	}]).
-	controller("TranslateController", ["$translate", "$window", "$scope", function ($translate, $window, $scope) {
-		$scope.select2Options = {
-	        allowClear:false
-	    };
-		
-		var lang = $window.navigator.userLanguage || $window.navigator.language;
-		$scope.lang = lang;
-		$translate.uses(lang);
-		
+	controller("TranslateController", ["$translate", "$scope", function ($translate, $scope) {
+		$scope.lang = "fr";
 		$scope.changeLanguage = function () {
 			$translate.uses($scope.lang);
 		}
